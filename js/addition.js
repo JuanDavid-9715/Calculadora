b1138e8598eaa20b047277344004bd9ba2880dd4
Juan David Medellin Calderon <medellincalderonjuandavid@gmail.com>
2022-12-17 00:10:31 -0500

    Creacion funcion addition

---
+++ js/addition.js
@@ -0,0 +1,8 @@
+exports.addition = function (numberA, numberB) {
+    let numA = parseInt(numberA);
+    let numB = parseInt(numberB);
+
+    const result = numA + numB;
+
+    return result.toString();
+};
